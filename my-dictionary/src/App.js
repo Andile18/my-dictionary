62d484e53082749af655d4b545dfb9eea8478a3d
Andile18 <zama.andile18@gmail.com>
2024-08-03 15:50:51 +0200

    Created dictionary app using html, css, & js

---
+++ my-dictionary/src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.png";
+import  "./App.css";
+import Dictionary from  "./Dictionary";
+
+export default function App() {
+  return (
+    <div className="container">
+    <div className="App">
+      <header className="App-header">
+       <img src={logo}
+       className="App-logo img-fluid"
+       alt="logo"  />
+     
+      </header>
+      <main> 
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+      <small> Coded by Zama Andile Dlamini
+      </small>
+     </footer>
+    </div>
+    </div>
+  );
+}
+
