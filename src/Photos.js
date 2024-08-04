ce8322969ec9a658645b3b123587bf4a0746197d
Andile18 <zama.andile18@gmail.com>
2024-08-04 22:41:27 +0200

    Update Photos.js

--- src/Photos.js
+++ src/Photos.js
@@ -14,7 +14,7 @@ export default function Photos(props){
                 return(    
                     <div className="col-4" key={index} >  
                     <a href={photo.src.original} target="_blank" rel="noreferrer">            
-                <img src={photo.src.landscape} className="img-fluid" alt="photo of word seach"/> 
+                <img src={photo.src.landscape} className="img-fluid" alt="word seach results"/> 
                 </a>
                 </div>
                 )
